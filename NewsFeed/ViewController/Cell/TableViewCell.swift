8f5030a0852c844aa6a8963dcfc52d10759276e4
KEKSoman <bassman7009@gmail.com>
2022-08-11 16:36:49 +0700

    Remove Storyboard and Xib

--- NewsFeed/ViewController/Cell/TableViewCell.swift
+++ NewsFeed/ViewController/Cell/TableViewCell.swift
@@ -8,19 +8,43 @@
 import UIKit
 
 class TableViewCell: UITableViewCell {
-
-    @IBOutlet weak var newsImage: UIImageView?
-    @IBOutlet weak var newsTitle: UILabel?
     
     
+    let newsImage = UIImageView() // 40x40, leading = 10, centerY
+    let newsTitle = UILabel() // trailing, leading = 16, centerY
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .clear
+        addSubview(newsImage)
+        newsImage.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            newsImage.widthAnchor.constraint(equalToConstant: 40),
+            newsImage.heightAnchor.constraint(equalToConstant: 40),
+            newsImage.leftAnchor.constraint(equalTo: leftAnchor, constant: 10),
+            newsImage.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ])
+        
+        addSubview(newsTitle)
+        newsTitle.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            newsTitle.leftAnchor.constraint(equalTo: newsImage.rightAnchor, constant: 16),
+            newsTitle.rightAnchor.constraint(equalTo: rightAnchor, constant: 16),
+            newsTitle.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ])
+    }
+    
+    private func setupCell() {
         self.selectionStyle = .none
-        self.backgroundColor = .white
-        
-        newsTitle?.textColor = .black
-        newsTitle?.font = UIFont.boldSystemFont(ofSize: 14)
-        
+        newsTitle.numberOfLines = 2
+        newsTitle.adjustsFontSizeToFitWidth = true
+        newsTitle.textColor = .black
+        newsTitle.font = UIFont.boldSystemFont(ofSize: 14)
+        newsImage.contentMode = .scaleAspectFit
+        newsImage.image = UIImage(named: "default.image")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
--- NewsFeed/ViewController/FeedViewController.swift
+++ NewsFeed/ViewController/FeedViewController.swift
@@ -7,20 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FeedViewController: UIViewController {
 
-    @IBOutlet weak var tableView: UITableView?
-    
-    let cell = "TableViewCell"
+    let tableView =  UITableView()
     var news: [NewsModel]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "News Feed"
+        self.view.addSubview(tableView)
+        tableView.frame = CGRect(origin: .zero, size: CGSize(width: self.view.frame.width, height: self.view.frame.height))
         NetWork.shared.loadNews(page: 1, count: 50)
-        
-        tableView?.register(UINib(nibName: cell, bundle: nil), forCellReuseIdentifier: cell)
-        tableView?.dataSource = self
-        tableView?.delegate = self
+        tableView.backgroundColor = .clear
+        tableView.register(TableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        tableView.delegate = self
         
         NotificationCenter.default.addObserver(self, selector: #selector(updateData), name: Notification.Name("update"), object: nil)        
     }
@@ -28,27 +30,27 @@ class ViewController: UIViewController {
     @objc private func updateData() {
         DispatchQueue.main.async {
             self.news = NetWork.shared.newsPack
-            self.tableView?.reloadData()
+            self.tableView.reloadData()
         }
     }
 }
 
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
+extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return news?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: cell, for: indexPath)
+        return tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
     }
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         guard let cell = cell as? TableViewCell else { return}
         let oneNews = news?[indexPath.row]
-        cell.newsTitle?.text = oneNews?.title
-        cell.newsImage?.loadImageUsingCache(urlString: oneNews?.titleImageUrl)
+        cell.newsTitle.text = oneNews?.title
+        cell.newsImage.loadImageUsingCache(urlString: oneNews?.titleImageUrl)
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
